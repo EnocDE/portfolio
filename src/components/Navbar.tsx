92b986ec0da4d546c0d8a89e6df048abba37b929
Enoc Dominguez <enocescazan@hotmail.com>
2024-06-18 13:36:08 -0600

    fix text-font

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <>
       <nav className="z-10 flex justify-between items-center absolute gap-5 top-0 left-0 right-0 py-5 animate-fade-in-down">
-        <h1 className="text-2xl font-bold">Enoc Dominguez</h1>
+        <h1 className="text-4xl font-bold font-caveat">Enoc Dominguez</h1>
         <ListSocialMedia />
       </nav>
     </>

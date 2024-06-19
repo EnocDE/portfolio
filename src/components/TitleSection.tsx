770e127080da32d649bdf02eaa58826592ca065a
Enoc Dominguez <enocescazan@hotmail.com>
2024-06-18 22:33:11 -0600

    fix title-font-size

--- src/components/TitleSection.tsx
+++ src/components/TitleSection.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function TitleSection({children} : {children: ReactNode}) {
   return (
-    <h2 className="py-10 text-6xl font-bold text-center">
+    <h2 className="py-10 text-4xl md:text-5xl font-bold text-center">
       {children}
     </h2>
   )

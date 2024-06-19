b2ae9a4b680bda62c5f264f4fcbfddbb43ce77e0
Enoc Dominguez <enocescazan@hotmail.com>
2024-06-19 15:18:10 -0600

    add link target

--- src/components/ProjectButton.tsx
+++ src/components/ProjectButton.tsx
@@ -1,17 +1,18 @@
-import { ReactNode } from "react"
+import { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
-  url: string
+  url: string;
 }
 
-export default function ProjectButton({children, url} : Props) {
+export default function ProjectButton({ children, url }: Props) {
   return (
     <a
-          className="shadow-animated mt-2 px-3 py-2 border-2 border-[#242424] hover:translate-x-[10px] hover:translate-y-[10px] hover:shadow-animated-reverse transition-all ease-linear rounded-lg dark:shadow-neutral-300 dark:border-neutral-300 font-semibold active:scale-95"
-          href={url}
-        >
-          {children}
-        </a>
-  )
+      className="shadow-animated mt-2 px-3 py-2 border-2 border-[#242424] hover:translate-x-[10px] hover:translate-y-[10px] hover:shadow-animated-reverse transition-all ease-linear rounded-lg dark:shadow-neutral-300 dark:border-neutral-300 font-semibold active:scale-95"
+      href={url}
+      target="_blank"
+    >
+      {children}
+    </a>
+  );
 }

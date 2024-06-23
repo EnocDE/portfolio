4f071712cb2e96ac5616979e38c3c9d7e3f16eb1
EnocDE <enocescazan@hotmail.com>
2024-06-22 23:21:12 -0600

    update hero description

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 export default function Hero() {
   return (
-    <section className="h-dvh">
-      <div className="grid md:grid-cols-2 h-full items-center gap-5 relative">
+    <section className="min-h-dvh">
+      <article className="grid md:grid-cols-2 h-full items-center gap-5 relative">
         <picture className="mt-20 md:mt-0 place-self-center self-end md:items-end md:place-content-end rounded-full md:rounded-none aspect-square md:w-full  md:h-full md:col-start-2 flex items-center overflow-hidden bg-slate-100 md:bg-transparent">
           <source srcSet="picture.webp" type="image/webp" />
           <img
@@ -11,6 +11,7 @@ export default function Hero() {
             loading="eager"
           />
         </picture>
+
         <div className="md:col-start-1 md:row-start-1 m-0 md:mt-52">
           <h1 className="text-4xl md:text-6xl font-bold animate-fade-in-left">
             Enoc Dominguez
@@ -19,18 +20,13 @@ export default function Hero() {
             Front-end Developer
           </h2>
           <p className="mt-5 animate-fade-in-up">
-            Hola! Soy un ingeniero en sistemas enfocado en el desarrollo web
-            frontend, especializado en crear interfaces intuitivas y atractivas
-            utilizando algunas tecnologías como HTML, CSS, JavaScript y
-            React.js. Aquí encontrarás ejemplos de mis proyectos donde combino
-            habilidades técnicas y creativas para ofrecer experiencias de
-            usuario excepcionales.
+            Hola, soy Enoc, un ingeniero en sistemas especializado en desarrollo web frontend. Me apasiona crear interfaces intuitivas y atractivas utilizando tecnologías como HTML, CSS, JavaScript y React.js. Aquí podrás explorar ejemplos de mis proyectos donde combino habilidades técnicas y creativas para ofrecer experiencias de usuario excepcionales.
           </p>
         </div>
 
         <div className="dark:brightness-[3.5] hidden md:block bg-shape w-full h-full bg-no-repeat opacity-5 absolute -z-10 bg-right-top"></div>
         <div className="dark:brightness-[3.5] hidden md:block bg-shape w-full h-full bg-no-repeat opacity-5 absolute -z-10 bg-left-bottom -ml-60 mt-60"></div>
-      </div>
+      </article>
     </section>
   );
 }

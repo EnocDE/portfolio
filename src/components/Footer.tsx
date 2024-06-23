ce5c31195cce267d42432690fb4b48bc22ea27ba
EnocDE <enocescazan@hotmail.com>
2024-06-22 23:05:23 -0600

    add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import Navbar from "./Navbar";
+
+export default function Footer() {
+  return (
+    <footer className="relative container-mx bg-teal-300 w-full">
+      <Navbar />
+    </footer>
+  )
+}

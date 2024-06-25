1d0620b71c53dc97165a52b360ca007028248961
Enoc Dominguez <enocescazan@hotmail.com>
2024-06-25 12:52:55 -0600

    style: update footer size and bg color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 
 export default function Footer() {
   return (
-    <footer className="relative container-mx bg-teal-300 w-full">
+    <footer className="relative container-mx w-full h-[80px]">
       <Navbar />
     </footer>
   )

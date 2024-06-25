f16e9deae0d368278bf2703864e844fb573a874b
Enoc Dominguez <enocescazan@hotmail.com>
2024-06-25 12:59:15 -0600

    style: change bg color on darkmode

--- src/components/ProjectButton.tsx
+++ src/components/ProjectButton.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function ProjectButton({ children, url }: Props) {
   return (
     <a
-      className="shadow-animated mt-2 px-3 py-2 border-2 border-[#242424] hover:translate-x-[10px] hover:translate-y-[10px] hover:shadow-animated-reverse transition-all ease-linear rounded-lg dark:shadow-neutral-300 dark:border-neutral-300 font-semibold active:scale-95"
+      className="shadow-animated mt-2 px-3 py-2 border-2 hover:translate-x-[10px] hover:translate-y-[10px] hover:shadow-animated-reverse transition-all ease-linear rounded-lg bg-white dark:bg-[#242424] dark:shadow-neutral-300 border-[#242424] dark:border-neutral-300 font-semibold active:scale-95"
       href={url}
       target="_blank"
     >
